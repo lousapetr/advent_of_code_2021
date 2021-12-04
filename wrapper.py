68376042d22cd983055427cbe275745894c01ea7
lousapetr <petr.lousa@datasentics.com>
2021-12-04 15:59:19 +0100

    create wrapper module

---
+++ wrapper.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+class Wrapper:
+
+    def __init__(self, day: int):
+        self.day = day
+        self.input_path = f'./inputs/{self.day:02d}_input.txt'
+        self.example_path = f'./inputs/{self.day:02d}_input_example.txt'
+        self.parser = None
+
+    def load_input(
+        self,
+        example: bool = False,
+        show: bool = True,
+        path: str = None
+    ):
+        if path is None:
+            if not example:
+                path = self.input_path
+            else:
+                path = self.example_path
+
+        loaded_input = self.parser(path)
+        if show:
+            print(loaded_input)
+        return loaded_input
+
+    def parse2list(self, path):
+        with open(path) as fp:
+            return fp.read().splitlines()
+
+    def parse2pandas(self, path):
+        df = pd.read_csv(path, delimiter=' ', header=None, names=None, dtype=int)
+        return df
+
+    def parse2intlist(self, path):
+        return [int(i) for i in self.list_parser(path)]
