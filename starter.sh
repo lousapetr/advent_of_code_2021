d03a793b9f257da4f8a04104b47912a2e38381f7
lousapetr <petr.lousa@datasentics.com>
2021-12-06 15:05:10 +0100

    create template solution

--- starter.sh
+++ starter.sh
@@ -69,43 +69,4 @@ then
 fi
 
 echo "Creating solution file ${code_file}"
-
-cat <<EOF >"${code_file}"
-import pandas as pd
-from wrapper import Wrapper
-
-# ${daily_url}
-
-
-class Solver(Wrapper):
-
-    def __init__(self, day: int):
-        super().__init__(day=day)
-        self.parser = 
-        self.input = super().load_input(example=False, show=False)
-        self.example = super().load_input(example=True)
-
-    def parse_custom(self, path):
-        pass
-
-    def task_1(self, input):
-        pass
-
-    def task_2(self, input):
-        pass
-
-
-solver = Solver(${day})
-
-print('=' * 15)
-print("Part 1 example:")
-print(solver.task_1(solver.example))  # == solution
-print("Part 1 solution:")
-print(solver.task_1(solver.input))
-
-print('=' * 15)
-print("Part 2 example:")
-print(solver.task_2(solver.example))  # == solution
-print("Part 2 solution:")
-print(solver.task_2(solver.input))
-EOF
+sed "s/DAY_NUMBER/${day}/" template.py > $code_file
