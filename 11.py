e7ce2abe2dd1c30243594a825fc8f6bd24d5063d
lousapetr <petr.lousa@datasentics.com>
2021-12-11 19:32:55 +0100

    solve part 1

--- 11.py
+++ 11.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from wrapper import Wrapper
+from scipy.signal import convolve2d
 
 # https://adventofcode.com/2021/day/11
 
@@ -9,15 +10,60 @@ class Solver(Wrapper):
 
     def __init__(self, day: int, example: bool, show=False):
         super().__init__(day=day)
-        self.parser = self.parse_custom
+        self.parser = self.parse2matrix
         self.example = example
         self.input = super().load_input(example=self.example, show=show)
+        self.octopi = self.input.copy()
+        self.reset()
 
     def parse_custom(self, path):
-        pass
+        line_list = self.parse2list(path)
+        matrix = [[int(i) for i in x] for x in line_list if x[0] != '#']
+        return np.array(matrix)
 
-    def task_1(self):
-        pass
+    def reset(self):
+        self.octopi = self.input.copy()
+
+    def flashes(self):
+        return self.octopi > 9
+
+    def energize(self, flash):
+        kernel = np.array([
+            [1, 1, 1],
+            [1, -100, 1],
+            [1, 1, 1]
+        ])
+        energy_boost = convolve2d(flash.astype(int), kernel, mode='same')
+        return energy_boost
+
+    def make_step(self):
+        self.octopi += 1
+        flash = self.flashes()
+        while np.any(flash):
+            # print(flash)
+            self.octopi[flash] = -100
+            energy_boost = self.energize(flash)
+            # print(energy_boost)
+            self.octopi += energy_boost
+            flash = self.flashes()
+            # print(self.octopi)
+        flashed = self.octopi < 0  # octopi that have flashed in this step
+        self.octopi[flashed] = 0
+        return flashed.sum()
+
+    def task_1(self, steps=100):
+        flashed_count = 0
+        print('Before any steps:')
+        print(self.matrix_str(self.octopi))
+        for i in range(steps):
+            flashed_count += self.make_step()
+            if (i < 10) or ((i + 1) % 10 == 0):
+                print(f'\nAfter {i + 1} steps:')
+                print(f'{flashed_count} octopi flashed yet.')
+                print(self.matrix_str(self.octopi))
+
+        print("\nResult:")
+        return flashed_count
 
     def task_2(self):
         pass
@@ -28,7 +74,7 @@ solver = Solver(11, example=True, show=True)
 
 print('=' * 15)
 print("Part 1:")
-print(solver.task_1())  # == example solution
+print(solver.task_1(steps=100))  # == 1656
 # print('=' * 15)
 # print("Part 2:")
 # print(solver.task_2())  # == example solution
--- wrapper.py
+++ wrapper.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from typing import List
 
 
 class Wrapper:
@@ -27,11 +29,14 @@ class Wrapper:
             print(loaded_input)
         return loaded_input
 
-    def parse2list(self, path):
+    def parse2list(self, path: str) -> List[str]:
         with open(path) as fp:
             return fp.read().splitlines()
 
-    def parse2pandas(self, path, **kwargs):
+    def parse2intlist(self, path: str) -> List[int]:
+        return [int(i) for i in self.parse2list(path)]
+
+    def parse2pandas(self, path: str, **kwargs) -> pd.DataFrame:
         kwargs.setdefault('delimiter', ' ')
         kwargs.setdefault('header', None)
         kwargs.setdefault('names', None)
@@ -39,5 +44,18 @@ class Wrapper:
         df = pd.read_csv(path, **kwargs)
         return df
 
-    def parse2intlist(self, path):
-        return [int(i) for i in self.parse2list(path)]
+    def parse2matrix(self, path: str) -> np.ndarray:
+        line_list = self.parse2list(path)
+        matrix = [[int(i) for i in x] for x in line_list if x[0] != '#']
+        return np.array(matrix)
+
+    def matrix_str(self, matrix: str, format: str = '1d') -> str:
+        """
+        Create string representation of matrix
+        """
+        return '\n'.join(
+            ''.join(
+                f'{num:{format}}' for num in row
+            ) for row in matrix
+        )
+
